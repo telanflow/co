672a870473d17041dfb13909177fa849e4d8b9a7
telanflow <telanflow@gmail.com>
2020-05-14 16:19:25 +0800

    优化文件路径函数

--- file.go
+++ file.go
@@ -4,24 +4,25 @@ import (
 	"bufio"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
+)
+
+const (
+	// sniff Length, use for detect file mime type
+	MimeSniffLen = 512
 )
 
 // 文件是否存在
 func FileExists(path string) bool {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true
-	}
-	if !os.IsNotExist(err) {
-		return true
-	}
-	return false
+	return PathExists(path)
 }
 
 // 给定path创建文件，如果目录不存在就递归创建
-func CreatFileNested(path string) (*os.File, error) {
+func FileCreateMust(path string) (*os.File, error) {
 	basePath := filepath.Dir(path)
 	if !FileExists(basePath) {
 		err := os.MkdirAll(basePath, 0700)
@@ -33,10 +34,41 @@ func CreatFileNested(path string) (*os.File, error) {
 	return os.Create(path)
 }
 
+// 获取文件mime类型
+// Usage:
+// 	file, err := os.Open(filepath)
+// 	if err != nil {
+// 		return
+// 	}
+//	mime := ReaderMimeType(file)
+func ReaderMimeType(r io.Reader) (mime string) {
+	var buf [MimeSniffLen]byte
+	n, _ := io.ReadFull(r, buf[:])
+	if n == 0 {
+		return ""
+	}
+
+	return http.DetectContentType(buf[:n])
+}
+
+// 获取文件mime类型. eg "image/png"
+func FileMimeType(path string) (mime string) {
+	if path == "" {
+		return
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+
+	return ReaderMimeType(file)
+}
+
 // 获取目录所有文件夹
-func GetPathDirs(dir string) (re []string) {
-	if FileExists(dir) {
-		files, _ := ioutil.ReadDir(dir)
+func GetDirs(path string) (re []string) {
+	if PathExists(path) {
+		files, _ := ioutil.ReadDir(path)
 		for _, f := range files {
 			if f.IsDir() {
 				re = append(re, f.Name())
@@ -47,9 +79,9 @@ func GetPathDirs(dir string) (re []string) {
 }
 
 // 获取目录所有文件
-func GetPathFiles(dir string) (re []string) {
-	if FileExists(dir) {
-		files, _ := ioutil.ReadDir(dir)
+func GetFiles(path string) (re []string) {
+	if PathExists(path) {
+		files, _ := ioutil.ReadDir(path)
 		for _, f := range files {
 			if !f.IsDir() {
 				re = append(re, f.Name())
@@ -59,28 +91,55 @@ func GetPathFiles(dir string) (re []string) {
 	return
 }
 
-// 按行读取文件
-func ReadLine(filePath string, callback func([]byte) bool) error {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	bfRd := bufio.NewReader(file)
-	for {
-		line, err := bfRd.ReadBytes('\n')
+// 获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤
+// 支持 Linux/macOS 软链接
+func GetFilesSuffix(dirPth, suffix string) (files []string, err error) {
+	files = make([]string, 0, 32)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	var walkFunc filepath.WalkFunc
+	walkFunc = func(filename string, fi os.FileInfo, err error) error { //遍历目录
 		if err != nil {
-			// 遇到任何错误立即返回，并忽略 EOF 错误信息
-			if err == io.EOF {
-				return nil
-			}
+			return err
+		}
+		if fi.IsDir() { // 忽略目录
+			return nil
+		}
+		if fi.Mode()&os.ModeSymlink != 0 { // 读取 symbol link
+			err = filepath.Walk(filename+string(os.PathSeparator), walkFunc)
 			return err
 		}
 
-		if !callback(line) {
-			// 返回 false 停止读取
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
+			files = append(files, path.Clean(filename))
+		}
+		return nil
+	}
+
+	err = filepath.Walk(dirPth, walkFunc)
+	return files, err
+}
+
+// 按行读取文件
+func ReadLine(f string) ([]string, error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	list := make([]string, 0)
+	bfRd := bufio.NewReader(file)
+	for {
+		line, err := bfRd.ReadBytes('\n')
+		// 遇到任何错误立即返回，并忽略 EOF 错误信息
+		if err == io.EOF {
 			break
 		}
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, string(line))
 	}
-	return nil
+	return list, nil
 }
--- path.go
+++ path.go
@@ -1,6 +1,7 @@
 package co
 
 import (
+	"os"
 	"path"
 	"strings"
 )
@@ -24,4 +25,44 @@ func PathRemoveSlash(path string) string {
 // 将path中的反斜杠'\'替换为'/'
 func PathFormSlash(old string) string {
 	return path.Clean(strings.ReplaceAll(old, "\\", "/"))
+}
+
+// 路径是否存在
+func PathExists(path string) bool {
+	if path == "" {
+		return false
+	}
+
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+// 判断路径是否为目录
+func IsDir(path string) bool {
+	if path == "" {
+		return false
+	}
+
+	if fi, err := os.Stat(path); err == nil {
+		return fi.IsDir()
+	}
+	return false
+}
+
+// 判断路径是否为文件
+func IsFile(path string) bool {
+	if path == "" {
+		return false
+	}
+
+	if fi, err := os.Stat(path); err == nil {
+		return !fi.IsDir()
+	}
+	return false
 }
